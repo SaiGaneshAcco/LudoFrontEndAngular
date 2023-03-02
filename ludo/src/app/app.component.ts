fab8dcff978bdf8d9315d517805e0089b46c2ce7
Mandalapu Sai Ganesh <sai.ganesh@accolite.com>
2023-03-02 21:47:37 +0530

    Changes for 6

--- ludo/src/app/app.component.ts
+++ ludo/src/app/app.component.ts
@@ -46,10 +46,10 @@ export class AppComponent {
   green_start = 26;
   blue_start = 0;
 
-  red_current = 13;
-  yellow_current = 39;
-  green_current = 26;
-  blue_current = 0;
+  red_current = -1;
+  yellow_current = -1;
+  green_current = -1;
+  blue_current = -1;
 
   blue_turn = true;
   red_turn = false;
@@ -66,111 +66,148 @@ export class AppComponent {
     this.yellow_current,
   ];
 
-  diceVal!: number;
+  dice!: number;
+  rollDice() {
+    this.dice = this.ludoService.diceValue();
+  }
   movePiece() {
-    let dice = this.ludoService.diceValue();
-    this.diceVal = dice;
     if (this.blue_turn) {
-      if (dice == 6 && this.blue_current == this.blue_start) {
-      }
-      let cellName;
-      console.log('Blue Dice value = ' + dice);
-      if (this.distance_traveled[0] + dice > 50) {
-        return this.checkHome(0, dice);
-      } else {
-        cellName = this.cellList[this.prev_val[0]];
-        document.getElementById(cellName)!.innerHTML = '';
-      }
-
-      this.blue_current = (this.blue_current + dice) % 52;
-
-      this.distance_traveled[0] += dice;
-      cellName = this.cellList[this.blue_current];
-      console.log('blue ', cellName);
-      document.getElementById(cellName)!.innerHTML = this.blue_span_ele;
-
-      this.prev_val[0] = this.blue_current;
+      this.rollDice();
+      console.log('Blue Dice value = ' + this.dice);
       this.blue_turn = false;
       this.red_turn = true;
-      return;
-    }
-    if (this.red_turn) {
-      let dice = this.ludoService.diceValue();
       let cellName;
-      console.log('Red Dice value = ' + dice);
-
-      if (this.distance_traveled[1] + dice > 50) {
-        return this.checkHome(1, dice);
+      if (this.dice != 6 && this.blue_current < 0) {
+        return;
+      } else if (this.dice == 6 && this.blue_current < 0) {
+        this.blue_current = this.blue_start;
+        this.prev_val[0] = this.blue_start;
+        cellName = this.cellList[this.blue_current];
+        console.log('blue ', cellName);
+        document.getElementById(cellName)!.innerHTML = this.blue_span_ele;
       } else {
-        cellName = this.cellList[this.prev_val[1]];
-        document.getElementById(cellName)!.innerHTML = '';
+        if (this.distance_traveled[0] + this.dice > 50) {
+          return this.checkHome(0, this.dice);
+        } else {
+          cellName = this.cellList[this.prev_val[0]];
+          document.getElementById(cellName)!.innerHTML = '';
+        }
+
+        this.blue_current = (this.blue_current + this.dice) % 52;
+
+        this.distance_traveled[0] += this.dice;
+        cellName = this.cellList[this.blue_current];
+        console.log('blue ', cellName);
+        document.getElementById(cellName)!.innerHTML = this.blue_span_ele;
+
+        this.prev_val[0] = this.blue_current;
+        return;
       }
+    }
 
-      this.red_current = (this.red_current + dice) % 52;
-      this.distance_traveled[1] += dice;
-      cellName = this.cellList[this.red_current];
-      console.log('red ', cellName);
-      document.getElementById(cellName)!.innerHTML = this.red_span_ele;
-
-      this.prev_val[1] = this.red_current;
+    if (this.red_turn) {
+      this.rollDice();
+      let cellName;
+      console.log('Red Dice value = ' + this.dice);
       this.green_turn = true;
       this.red_turn = false;
-      return;
-    }
-    if (this.green_turn) {
-      let dice = this.ludoService.diceValue();
-      let cellName;
-
-      console.log('Green Dice value = ' + dice);
-      if (this.distance_traveled[2] + dice > 50) {
-        return this.checkHome(2, dice);
+      if (this.dice != 6 && this.red_current < 0) {
+        return;
+      } else if (this.dice == 6 && this.red_current < 0) {
+        this.red_current = this.red_start;
+        this.prev_val[1] = this.red_start;
+        cellName = this.cellList[this.red_current];
+        console.log('red ', cellName);
+        document.getElementById(cellName)!.innerHTML = this.red_span_ele;
       } else {
-        cellName = this.cellList[this.prev_val[2]];
-        document.getElementById(cellName)!.innerHTML = '';
+        if (this.distance_traveled[1] + this.dice > 50) {
+          return this.checkHome(1, this.dice);
+        } else {
+          cellName = this.cellList[this.prev_val[1]];
+          document.getElementById(cellName)!.innerHTML = '';
+        }
+
+        this.red_current = (this.red_current + this.dice) % 52;
+        this.distance_traveled[1] += this.dice;
+        cellName = this.cellList[this.red_current];
+        console.log('red ', cellName);
+        document.getElementById(cellName)!.innerHTML = this.red_span_ele;
+
+        this.prev_val[1] = this.red_current;
+        return;
       }
+    }
 
-      this.green_current = (this.green_current + dice) % 52;
-      this.distance_traveled[2] += dice;
-      cellName = this.cellList[this.green_current];
-      console.log('green ', cellName);
-      document.getElementById(cellName)!.innerHTML = this.green_span_ele;
-
-      this.prev_val[2] = this.green_current;
+    if (this.green_turn) {
+      this.rollDice();
+      let cellName;
+      console.log('Green Dice value = ' + this.dice);
       this.yellow_turn = true;
       this.green_turn = false;
-      return;
+      if (this.dice != 6 && this.green_current < 0) {
+        return;
+      } else if (this.dice == 6 && this.green_current < 0) {
+        this.green_current = this.green_start;
+        this.prev_val[2] = this.green_start;
+        cellName = this.cellList[this.green_current];
+        console.log('green ', cellName);
+        document.getElementById(cellName)!.innerHTML = this.green_span_ele;
+      } else {
+        if (this.distance_traveled[2] + this.dice > 50) {
+          return this.checkHome(2, this.dice);
+        } else {
+          cellName = this.cellList[this.prev_val[2]];
+          document.getElementById(cellName)!.innerHTML = '';
+        }
+
+        this.green_current = (this.green_current + this.dice) % 52;
+        this.distance_traveled[2] += this.dice;
+        cellName = this.cellList[this.green_current];
+        console.log('green ', cellName);
+        document.getElementById(cellName)!.innerHTML = this.green_span_ele;
+
+        this.prev_val[2] = this.green_current;
+        return;
+      }
     }
     if (this.yellow_turn) {
-      let dice = this.ludoService.diceValue();
+      this.rollDice();
       let cellName;
-
-      console.log('Yellow Dice value = ' + dice);
-      if (this.distance_traveled[3] + dice > 50) {
-        return this.checkHome(3, dice);
-      } else {
-        cellName = this.cellList[this.prev_val[3]];
-        document.getElementById(cellName)!.innerHTML = '';
-      }
-
-      this.yellow_current = (this.yellow_current + dice) % 52;
-      this.distance_traveled[3] += dice;
-      cellName = this.cellList[this.yellow_current];
-      console.log('yellow ', cellName);
-      document.getElementById(cellName)!.innerHTML = this.yellow_span_ele;
-
-      this.prev_val[3] = this.yellow_current;
+      console.log('Yellow Dice value = ' + this.dice);
       this.yellow_turn = false;
       this.blue_turn = true;
-      console.log(this.distance_traveled);
-      return;
+      if (this.dice != 6 && this.yellow_current < 0) {
+        return;
+      } else if (this.dice == 6 && this.yellow_current < 0) {
+        this.yellow_current = this.yellow_start;
+        this.prev_val[3] = this.yellow_start;
+        cellName = this.cellList[this.yellow_current];
+        console.log('yellow ', cellName);
+        document.getElementById(cellName)!.innerHTML = this.yellow_span_ele;
+      } else {
+        if (this.distance_traveled[3] + this.dice > 50) {
+          return this.checkHome(3, this.dice);
+        } else {
+          cellName = this.cellList[this.prev_val[3]];
+          document.getElementById(cellName)!.innerHTML = '';
+        }
+
+        this.yellow_current = (this.yellow_current + this.dice) % 52;
+        this.distance_traveled[3] += this.dice;
+        cellName = this.cellList[this.yellow_current];
+        console.log('yellow ', cellName);
+        document.getElementById(cellName)!.innerHTML = this.yellow_span_ele;
+
+        this.prev_val[3] = this.yellow_current;
+        console.log(this.distance_traveled);
+        return;
+      }
     }
   }
 
   checkHome(c: number, dice: number) {
     if (c == 0) {
       let cellCount = this.distance_traveled[0] + dice;
-      this.blue_turn = false;
-      this.red_turn = true;
       let newCellName;
       if (cellCount > 56) {
         return;
@@ -200,27 +237,23 @@ export class AppComponent {
     if (c == 1) {
       let cellCount = this.distance_traveled[1] + dice;
       let newCellName;
-      this.red_turn = false;
-      this.green_turn = true;
       if (cellCount > 56) {
         return;
       } else if (cellCount == 56) {
         this.buttonEnabled = !this.buttonEnabled;
-        newCellName =
-          this.ludoService.redHomePath[this.distance_traveled[1]];
+        newCellName = this.ludoService.redHomePath[this.distance_traveled[1]];
         document.getElementById(newCellName)!.innerHTML = '';
         setTimeout(() => {
           alert('Player 2 wins');
         }, 500);
       } else {
         if (this.distance_traveled[1] > 50) {
-        newCellName =
-          this.ludoService.redHomePath[this.distance_traveled[1]];
-        document.getElementById(newCellName)!.innerHTML = '';
-      } else {
-        newCellName = this.cellList[this.prev_val[1]];
-        document.getElementById(newCellName)!.innerHTML = '';
-      }
+          newCellName = this.ludoService.redHomePath[this.distance_traveled[1]];
+          document.getElementById(newCellName)!.innerHTML = '';
+        } else {
+          newCellName = this.cellList[this.prev_val[1]];
+          document.getElementById(newCellName)!.innerHTML = '';
+        }
         this.distance_traveled[1] = cellCount;
         newCellName = this.ludoService.redHomePath[cellCount];
         document.getElementById(newCellName)!.innerHTML = this.red_span_ele;
@@ -230,15 +263,12 @@ export class AppComponent {
     }
     if (c == 2) {
       let cellCount = this.distance_traveled[2] + dice;
-      this.green_turn = false;
-      this.yellow_turn = true;
       let newCellName;
       if (cellCount > 56) {
         return;
       } else if (cellCount == 56) {
         this.buttonEnabled = !this.buttonEnabled;
-        newCellName =
-          this.ludoService.greenHomePath[this.distance_traveled[2]];
+        newCellName = this.ludoService.greenHomePath[this.distance_traveled[2]];
         document.getElementById(newCellName)!.innerHTML = '';
         setTimeout(() => {
           alert('Player 3 wins');
@@ -261,8 +291,6 @@ export class AppComponent {
     }
     if (c == 3) {
       let cellCount = this.distance_traveled[3] + dice;
-      this.yellow_turn = false;
-      this.blue_turn = true;
       let newCellName;
       if (cellCount > 56) {
         return;
